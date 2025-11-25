9b2c6df8fcdae5945c39a9861693fd97ae32e669
Abhinandan Hongal <abhinandan_hongal@thbs.com>
2025-11-25 11:53:32 +0530

    updated the test suite for orderSim

--- cypress/e2e/pages/orderSimsPage.js
+++ cypress/e2e/pages/orderSimsPage.js
@@ -18,7 +18,7 @@ class OrderSimsPage {
 
   // Visit Order Sims page directly
   visit() {
-    cy.visit('https://retail-staging.48.ie/order-sims');
+    cy.visit('/order-sims');
   }
 
   // Select retail store name from dropdown
